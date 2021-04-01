8569d9a07805224885c27fabbf745f1c354c37be
AwesomeAuuki <79605349+AwesomeAuuki@users.noreply.github.com>
2021-04-01 17:22:30 -0500

    goodbye, world.

--- src/main/kotlin/kvm/pack/Terminal.kt
+++ src/main/kotlin/kvm/pack/Terminal.kt
@@ -8,29 +8,9 @@ import java.net.URL
 import kotlin.collections.ArrayList
 import kotlin.system.exitProcess
 
-class Response {
-    var responses: ArrayList<String> = arrayListOf()
-    var fitness: Int? = 0
-}
+data class Response(var bad: HashSet<String> = HashSet(), var good: HashSet<String> = HashSet(), var fitness: Int = 0)
 
-
-data class Responses(val good: ArrayList<String>, val  bad: ArrayList<String>)
-
-// Creates kvm.pack.person class
-fun person(lambda: Person.() -> Unit): Person {
-    val person = Person()
-    person.apply(lambda)
-    return person
-}
-
-// Declaring class
-var person = person {}
-
-class Person {
-    var name: String = ""
-}
-
-private val allResponses = arrayListOf<Response>()
+private val allResponses = hashSetOf<Response>()
 fun response(lambda: Response.() -> Unit): Response {
     val response = Response()
     response.apply(lambda)
@@ -38,36 +18,40 @@ fun response(lambda: Response.() -> Unit): Response {
 }
 
 // Loads & Reads JSON Configuration
-fun loadConfig() {
-    val bufferedReader = URL("https://raw.githubusercontent.com/TechnoInventor/TherapyKt/master/messages.json").readText()
-    val response = Gson().fromJson(bufferedReader, Responses::class.java)
-    allResponses.addAll(arrayListOf(response {
-        responses = response.bad
-        fitness = 1
-    }, response {
-        responses = response.good
-        fitness = 0
-    }))
-}
+fun loadConfig() =
+    URL("https://raw.githubusercontent.com/TechnoInventor/TherapyKt/master/messages.json").readText().let {
+        Gson().fromJson(it, Response::class.java).let { resp ->
+            allResponses += arrayListOf(
+                response {
+                    bad = resp.bad
+                    fitness = 1
+                }, response {
+                    bad = resp.good
+                    fitness = 0
+                }
+            )
+        }
+    }
 
 // Name input, loops if no kvm.pack.response
 fun createName() {
-    val name = "Please enter a name!".newLine()
-    if (!name.isBlank()) {
-        person.name = name
-        "Great to meet cha, ${person.name}".print()
+    "Please enter a name!".newLine().let {
+        if (it.isBlank()) createName() else "Great to meet you, $it".print()
     }
 }
 
 // Asks how you're doing. Loops if you don't say yes or no
 fun askMood() {
-    val moodResponse = "So, How is everything?".newLine()
-    val moodFilter = allResponses.filter { it.responses.contains(moodResponse) }
-    if (moodFilter.isNullOrEmpty()) {
-        "Please answer with yes or no".print()
-        askMood()
+    "So, How is everything?".newLine().let { result ->
+        allResponses.firstOrNull { it.good.contains(result) || it.bad.contains(result) }.let {
+            if (it == null) {
+                "Please answer with yes or no".print()
+                askMood()
+                return
+            }
+            if (it.fitness == 0) goodMood() else badMood()
+        }
     }
-    moodFilter.forEach { if (it.fitness == 0) goodMood() else badMood() }
 }
 
 fun String.breakPause() {
@@ -76,31 +60,22 @@ fun String.breakPause() {
 }
 
 // Leaves a happy message if user is okay
-fun goodMood() {
-    "That's so awesome to hear! I hope the rest of your day stays well!".breakPause()
+fun goodMood(): Unit = "That's so awesome to hear! I hope the rest of your day stays well!".breakPause().let {
     exitProcess(0)
 }
 
 // Moves forward with program to determine what's wrong with the user
-fun badMood() {
-    val resp = """Oh, I'm sorry to hear. Would you like to vent?
+fun badMood() = """Oh, I'm sorry to hear. Would you like to vent?
 Venting is useful when you're in a bad state of mind.
 It's better to let stress out in a positive way, like writing or
 singing. So how about it? Would you like to type about it?
 I'm all ears!
-yes or no?""".newLine()
-    when (resp) {
-        "yes" -> {
-            "Great! Please make sure to type exit when you're done.\n\n".print()
-            freeText()
-        }
-        "no" -> badMoodCont()
-        else -> {
-            "Please type yes or no\n\n".breakPause()
-            badMood()
-        }
+yes or no?""".newLine().let {
+    if (it == "yes") {
+        "Great! Please make sure to type exit when you're done.\n\n".print()
+        freeText()
     }
-
+    else badMoodCont()
 }
 
 val lines = arrayListOf<String>()
@@ -164,17 +139,13 @@ don't hesitate. Call someone. Let someone know how you feel. The situation will
 only get worse if you don't speak up. I encourage you to find help!
     """.print()
 
-    arrayListOf(
-        "Thank you for using the program! Please leave feedback by emailing\n" +
-                "migsterfixer@gmail.com", "Press Enter To Exit: "
-    ).sendLines()
+    arrayListOf("Thank you for using the program! Please leave feedback by emailing\n" +
+            "migsterfixer@gmail.com", "Press Enter To Exit: ").sendLines()
 }
 
 // Everyone knows what this does
 fun main() {
     loadConfig()
-    do {
-        createName()
-    } while (person.name.isBlank())
+    createName()
     askMood()
 }
